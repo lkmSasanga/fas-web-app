20fbe167162d0df0867f81c04937e79142d80bf4
lkmSasanga <lkmsasanga@gmail.com>
2021-01-26 23:46:43 +0530

    cap progressbar labels

--- src/components/ProgreessBars/ProgressBars.js
+++ src/components/ProgreessBars/ProgressBars.js
@@ -5,36 +5,11 @@ import ProgressBar from "./ProgressBar/ProgressBar";
 class ProgressBars extends Component {
     state = {
         loaded: false,
-        capitalizedFeatureOne: '',
-        capitalizedFeatureTwo: '',
-        capitalizedFeatureThree: ''
     }
     componentDidMount() {
         if (this.props.features) {
             this.setState({loaded: true})
         }
-    }
-
-    capitalizeHandler = () => {
-        console.log('from progress bars',this.props.features.featureOne.name)
-
-        let featureOne = String(this.props.features.featureOne.name)
-        let capitalizedFeatureOne = featureOne.charAt(0).toUpperCase() + featureOne.slice(1)
-
-
-        const featureTwo = String(this.props.features.featureTwo.name)
-        const capitalizedFeatureTwo = featureTwo.charAt(0).toUpperCase() + featureTwo.slice(1)
-
-        const featureThree = String(this.props.features.featureThree.name)
-        const capitalizedFeatureThree = featureThree.charAt(0).toUpperCase() + featureThree.slice(1)
-
-        this.setState({
-            capitalizedFeatureOne: capitalizedFeatureOne,
-            capitalizedFeatureTwo: capitalizedFeatureTwo,
-            capitalizedFeatureThree: capitalizedFeatureThree
-        })
-
-        console.log('testing..', this.state.capitalizedFeatureOne)
     }
 
     render() {
@@ -47,21 +22,30 @@ class ProgressBars extends Component {
                     <p className={classes.PieChartTitle}>Feature Report</p>
                     <br/>
                     <div>
-                        <p className={classes.FeatureLabel}>{this.props.features.featureOne.name}</p>
+                        <p className={classes.FeatureLabel}>{
+                            String(this.props.features.featureOne.name).charAt(0).toUpperCase() +
+                            String(this.props.features.featureOne.name).slice(1)
+                        }</p>
                         <div className={classes.ProgressBar}>
                             <ProgressBar battery={this.props.features.featureOne.positive}/>
                         </div>
                     </div>
 
                     <div>
-                        <p className={classes.FeatureLabel}>{this.props.features.featureTwo.name}</p>
+                        <p className={classes.FeatureLabel}>{
+                            String(this.props.features.featureTwo.name).charAt(0).toUpperCase() +
+                            String(this.props.features.featureTwo.name).slice(1)
+                        }</p>
                         <div className={classes.ProgressBar}>
                             <ProgressBar battery={this.props.features.featureTwo.positive}/>
                         </div>
                     </div>
 
                     <div>
-                        <p className={classes.FeatureLabel}>{this.props.features.featureThree.name}</p>
+                        <p className={classes.FeatureLabel}>{
+                            String(this.props.features.featureThree.name).charAt(0).toUpperCase() +
+                            String(this.props.features.featureThree.name).slice(1)
+                        }</p>
                         <div className={classes.ProgressBar}>
                             <ProgressBar battery={this.props.features.featureThree.positive}/>
                         </div>
