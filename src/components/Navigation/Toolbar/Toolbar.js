a5bd3e3875dab5b7d775f0cd7bdc0c4690526be5
lkmSasanga <lkmsasanga@gmail.com>
2021-02-07 18:18:34 +0530

    toolbar removed

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,8 +8,8 @@ import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 const toolbar = (props) => {
     return(
         <header className={classes.Toolbar}>
-            <DrawerToggle clicked={props.drawerToggleClicked}/>
-            <Logo height="80%"/>
+            {/*<DrawerToggle clicked={props.drawerToggleClicked}/>*/}
+            {/*<Logo height="80%"/>*/}
             {/*<nav className={classes.DesktopOnly}>*/}
             {/*    <NavigationItems/>*/}
             {/*</nav>*/}
