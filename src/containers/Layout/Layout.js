50571bf4a07d7423ee6345f0342ac13958a81a41
lkmSasanga <lkmsasanga@gmail.com>
2021-02-08 14:23:49 +0530

    button color changed

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -24,9 +24,9 @@ class Layout extends Component {
         return (
             <React.Fragment>
                 <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-                <SideDrawer
-                    open={this.state.showSideDrawer}
-                    closed={this.sideDrawerClosedHandler}/>
+                {/*<SideDrawer*/}
+                {/*    open={this.state.showSideDrawer}*/}
+                {/*    closed={this.sideDrawerClosedHandler}/>*/}
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
