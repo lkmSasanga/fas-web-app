e8a28e07d76fba3eded2d2b254c9c889dfc1cd8b
lkmSasanga <lkmsasanga@gmail.com>
2021-02-14 11:43:18 +0530

    signup folder removed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React, {Component} from 'react';
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 import './App.css';
-// import Layout from './containers/Layout/Layout';
 import InputControl from './containers/InputControl/InputControl';
 import Login from "./containers/Login/Login";
-// import SignUp from "./containers/Login/SignUp/Signup";
 
 class App extends Component{
     render() {
@@ -13,13 +11,10 @@ class App extends Component{
             <BrowserRouter>
                 <Route>
                     <div className="App">
-                        {/*<Layout>*/}
                             <Switch>
                                 <Route path="/" component={Login}/>
-                                {/*<Route path="/signup" component={SignUp}/>*/}
                                 <Route path="/search" component={InputControl}/>
                             </Switch>
-                        {/*</Layout>*/}
                     </div>
                 </Route>
             </BrowserRouter>
