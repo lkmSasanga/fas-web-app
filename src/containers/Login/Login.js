142a82d847ee09bdda0a8a1fd769f23a619611d4
lkmSasanga <lkmsasanga@gmail.com>
2021-05-24 11:41:05 +0530

    refactored code login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -44,32 +44,58 @@ class Login extends Component {
     SignUpClickHandler = () => {
         if(this.state.loadSignup) {
             if(this.state.email === '' && this.state.password === '' && this.state.username === '') {
-                return this.setState({ emailPlaceHolder: 'Please enter your email', passwordPlaceHolder: 'Please enter your password', usernamePlaceHolder: 'Please enter a username' })
+                return this.setState({
+                    emailPlaceHolder: 'Please enter your email',
+                    passwordPlaceHolder: 'Please enter your password',
+                    usernamePlaceHolder: 'Please enter a username' })
             } else if(this.state.username === ''){
-                this.setState({ usernamePlaceHolder: 'Please enter a username' })
+                this.setState({
+                    usernamePlaceHolder: 'Please enter a username'
+                })
             } else if(this.state.email === ''){
-                this.setState({ emailPlaceHolder: 'Please enter your email' })
+                this.setState({
+                    emailPlaceHolder: 'Please enter your email'
+                })
             } else if(this.state.password === ''){
-                this.setState({ passwordPlaceHolder: 'Please enter your password' })
+                this.setState({
+                    passwordPlaceHolder: 'Please enter your password'
+                })
             }
         }
 
-        this.setState({loadSignup: true, submitButtonName: 'Signup', errorOccurs: false})
+        this.setState({
+            loadSignup: true,
+            submitButtonName: 'Signup',
+            errorOccurs: false
+        })
 
     }
 
     onClickHandler = (e) => {
         e.preventDefault()
         if(this.state.email === '' && this.state.password === '') {
-            return this.setState({ emailPlaceHolder: 'Please enter your email', passwordPlaceHolder: 'Please enter your password' })
+            return this.setState({
+                emailPlaceHolder: 'Please enter your email',
+                passwordPlaceHolder: 'Please enter your password'
+            })
         } else if(this.state.password === ''){
-            this.setState({ passwordPlaceHolder: 'Please enter your password' })
+            this.setState({
+                passwordPlaceHolder: 'Please enter your password'
+            })
         } else if(this.state.email === ''){
-            this.setState({ emailPlaceHolder: 'Please enter your email' })
+            this.setState({
+                emailPlaceHolder: 'Please enter your email'
+            })
         } else if(this.state.username === ''){
-            this.setState({ usernamePlaceHolder: 'Please enter a username' })
+            this.setState({
+                usernamePlaceHolder: 'Please enter a username'
+            })
         } else if(this.state.email && this.state.password) {
-            this.setState({ clicked: true, isLoading: true, errorOccurs: false })
+            this.setState({
+                clicked: true,
+                isLoading: true,
+                errorOccurs: false
+            })
 
             if (!this.state.loadSignup){
                 // console.log('loadSignup...', this.state.loadSignup)
@@ -214,7 +240,13 @@ class Login extends Component {
                                     {this.state.loadSignup ?
                                         <React.Fragment>
                                             <div className={Classes.field}>
-                                                <input type="text" required="required" name="username" autoComplete="on" onChange={e => this.onChangeUsername(e)}/>
+                                                <input
+                                                    type="text"
+                                                    required="required"
+                                                    name="username"
+                                                    autoComplete="on"
+                                                    onChange={e => this.onChangeUsername(e)}
+                                                />
                                                 <label>{this.state.usernamePlaceHolder}</label>
                                             </div>
                                             <div className={Classes.field}>
@@ -229,7 +261,13 @@ class Login extends Component {
                                                     :<label>Email Address</label> }
                                             </div>
                                             <div className={Classes.field}>
-                                                <input type="password" required="required" name="password" autoComplete="on" onChange={e => this.onChangePassword(e)}/>
+                                                <input
+                                                    type="password"
+                                                    required="required"
+                                                    name="password"
+                                                    autoComplete="on"
+                                                    onChange={e => this.onChangePassword(e)}
+                                                />
                                                 <label>Password</label>
                                             </div>
                                         </React.Fragment> : null
@@ -245,7 +283,11 @@ class Login extends Component {
                                     </div>
 
                                     <div className={Classes.field}>
-                                        <input type="submit" value={this.state.submitButtonName} onClick={e => this.onClickHandler(e)}/>
+                                        <input
+                                            type="submit"
+                                            value={this.state.submitButtonName}
+                                            onClick={e => this.onClickHandler(e)}
+                                        />
                                     </div>
                                     {!this.state.loadSignup ?
                                         <div className={Classes.signupLink}>
