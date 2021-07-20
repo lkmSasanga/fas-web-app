719713e33ee08d12e2c2a70e34c94fd5ef11ae88
lkmSasanga <lkmsasanga@gmail.com>
2021-07-20 17:24:50 +0530

    code reformated 1.0

--- src/components/Charts/DonetChart/DonetChart.js
+++ src/components/Charts/DonetChart/DonetChart.js
@@ -1,153 +1,149 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import classes from './DonetChart.module.css';
+import classes from "./DonetChart.module.css";
 
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 import ProgressBars from "../../ProgreessBars/ProgressBars";
-// import NegComments from '../../Comments/NegComments/NegComments'
-import FeatureOne from '../../Comments/FeatureOne/FeatureOne'
-import FeatureTwo from '../../Comments/FeatureTwo/FeatureTwo'
-import FeatureThree from '../../Comments/FeatureThree/FeatureThree'
 
-class DonetChart extends Component{
-    state = {
-        dataRecieved: false,
-        chartSize: 80,
-        mainChartData: {
-            pos: this.props.itemDetails.positive,
-            neg: this.props.itemDetails.negative
-        },
-        featureOne: {
-            name: this.props.itemDetails.features,
-            pos: null,
-            neg: null
-        },
-        featureTwo: {
-            name: '',
-            pos: null,
-            neg: null
-        },
-        featuresArray: [],
-        features: {}
+class DonetChart extends Component {
+  state = {
+    dataRecieved: false,
+    chartSize: 80,
+    mainChartData: {
+      pos: this.props.itemDetails.positive,
+      neg: this.props.itemDetails.negative,
+    },
+    featureOne: {
+      name: this.props.itemDetails.features,
+      pos: null,
+      neg: null,
+    },
+    featureTwo: {
+      name: "",
+      pos: null,
+      neg: null,
+    },
+    featuresArray: [],
+    features: {},
+  };
+
+  componentDidMount() {
+    this.setState({ dataRecieved: true });
+    this.setState({ features: this.props.itemDetails.features });
+
+    const featuresArray = [];
+    for (let feature in this.state.features) {
+      featuresArray.push({
+        name: this.state.features[feature].name,
+        pos: this.state.features[feature].positive,
+        neg: this.state.features[feature].negative,
+      });
     }
+    this.setState({ featuresArray: featuresArray });
 
-    componentDidMount() {
-        this.setState({dataRecieved: true})
-        this.setState({features: this.props.itemDetails.features})
+    // console.log('features array ', this.state.featuresArray[0].name)
+    // console.log('features array ', this.state.featuresArray[0].pos)
+    // console.log('features array ', this.state.featuresArray[0].neg)
 
-        const featuresArray = [];
-        for (let feature in this.state.features) {
-            featuresArray.push({
-                name: this.state.features[feature].name,
-                pos: this.state.features[feature].positive,
-                neg: this.state.features[feature].negative
-            })
-        }
-        this.setState({featuresArray: featuresArray})
+    console.log(this.state.features);
+  }
 
-        // console.log('features array ', this.state.featuresArray[0].name)
-        // console.log('features array ', this.state.featuresArray[0].pos)
-        // console.log('features array ', this.state.featuresArray[0].neg)
+  render(props) {
+    let pos = parseInt(this.props.itemDetails.positive);
+    let neg = parseInt(this.props.itemDetails.negative);
 
-        console.log(this.state.features)
-
-    }
-
-    render (props) {
-
-        let pos = parseInt(this.props.itemDetails.positive)
-        let neg = parseInt(this.props.itemDetails.negative)
-
-        const chartComponent = {
-        chart: {
-            marginTop: 0,
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: 0,
-            height: 330,
-            width: 300,
-            // plotBackgroundColor: '#e1ffff',
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: "pie",
+    const chartComponent = {
+      chart: {
+        marginTop: 0,
+        marginBottom: 0,
+        marginLeft: 0,
+        marginRight: 0,
+        height: 330,
+        width: 300,
+        // plotBackgroundColor: '#e1ffff',
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: "pie",
+        style: {
+          float: "right",
+          // 'font-family': 'Poppins, sans serif',
+        },
+      },
+      tooltip: {
+        pointFormat: "<b>{point.y} %</b>",
+      },
+      plotOptions: {
+        pie: {
+          showInLegend: true,
+          innerSize: "60%",
+          align: "right",
+          verticalAlign: "center",
+          itemMarginTop: 10,
+          dataLabels: {
+            enabled: false,
+            distance: -14,
+            color: "white",
             style: {
-                'float': 'right',
-                // 'font-family': 'Poppins, sans serif',
-    }
-
-        },
-        tooltip: {
-            pointFormat: "<b>{point.y} %</b>"
-        },
-        plotOptions: {
-            pie: {
-                showInLegend: true,
-                    innerSize: "60%",
-                    align: "right",
-                    verticalAlign: "center",
-                    itemMarginTop: 10,
-                    dataLabels: {
-                    enabled: false,
-                        distance: -14,
-                        color: "white",
-                        style: {
-                        fontWeight: "bold",
-                        fontsize: 50
-                    }
-                },
-                size: `${this.state.chartSize}%`
-            }
-        },
-        series: [{
-            name: 'Sentiment',
-            colorByPoint: true,
-            data: [{
-                name: 'Positive',
-                y: pos,
-                color: '#41b84b',
-
+              fontWeight: "bold",
+              fontsize: 50,
             },
-                {
-                    name: 'Negative',
-                    y: neg,
-                    color: '#d22d48'
-                }
-            ]
-        }],
-            title: {
-            // text: `Sentiment Analyzed data of ${this.props.itemDetails.item}`
-            text: ``
+          },
+          size: `${this.state.chartSize}%`,
         },
-        subtitle: {
-            text: pos + '%' ,
-                style: {
-                fontSize: 14,
-                    fontWeight: "bold",
-                    align: 'center',
-                    floating: true,
-                    color: "#000000"
+      },
+      series: [
+        {
+          name: "Sentiment",
+          colorByPoint: true,
+          data: [
+            {
+              name: "Positive",
+              y: pos,
+              color: "#41b84b",
             },
-            y: 160
+            {
+              name: "Negative",
+              y: neg,
+              color: "#d22d48",
+            },
+          ],
         },
-            responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 499
-                    },
-                    // chartOptions: {
-                    //     align: 'center',
-                    //     size: '90%',
-                    // },
-                    plotOptions: {
-                        pie: {
-                            size: '90%'
-                        }
-                    },
-                }]
-            }
-
-    }
+      ],
+      title: {
+        // text: `Sentiment Analyzed data of ${this.props.itemDetails.item}`
+        text: ``,
+      },
+      subtitle: {
+        text: pos + "%",
+        style: {
+          fontSize: 14,
+          fontWeight: "bold",
+          align: "center",
+          floating: true,
+          color: "#000000",
+        },
+        y: 160,
+      },
+      responsive: {
+        rules: [
+          {
+            condition: {
+              maxWidth: 499,
+            },
+            // chartOptions: {
+            //     align: 'center',
+            //     size: '90%',
+            // },
+            plotOptions: {
+              pie: {
+                size: "90%",
+              },
+            },
+          },
+        ],
+      },
+    };
     // let chartView = this.props.itemDetails ?
     //
     // if (this.props.itemDetails) {
@@ -161,53 +157,43 @@ class DonetChart extends Component{
 
     // console.log('from donetChart ', this.props.itemDetails.item)
 
+    let chartsStock = (
+      <div className={classes.DonetChart}>
+        <HighchartsReact highcharts={Highcharts} options={chartComponent} />
+      </div>
+    );
 
-        let chartsStock = (
-            <div className={classes.DonetChart}>
-                <HighchartsReact
-                    highcharts={Highcharts}
-                    options={chartComponent}
-                />
-            </div>
-        )
+    const itemName = String(this.props.itemDetails.item);
+    const capitalizedName =
+      itemName.charAt(0).toUpperCase() + itemName.slice(1);
 
-        const itemName = String(this.props.itemDetails.item)
-        const capitalizedName = itemName.charAt(0).toUpperCase() + itemName.slice(1)
+    return (
+      <React.Fragment>
+        <div className={classes.ChartsRow}>
+          <div className={classes.Cards}>
+            <p className={classes.PieChartTitle}>Overall Report</p>
+            <p className={classes.PieChartItemName}>{capitalizedName}</p>
+            <div className={classes.PieChart}>{chartsStock}</div>
+          </div>
+          <ProgressBars features={this.props.itemDetails.features} />
+        </div>
 
-        return (
-            <React.Fragment>
-                <div className={classes.ChartsRow}>
-                    <div className={classes.Cards}>
-                        <p className={classes.PieChartTitle}>Overall Report</p>
-                        <p className={classes.PieChartItemName}>{capitalizedName}</p>
-                        <div className={classes.PieChart}>{chartsStock}</div>
-                    </div>
-                    <ProgressBars features={this.props.itemDetails.features}/>
-                </div>
+        {/*<div className={classes.FeedbackCardsRow}>*/}
+        {/*    <div className={classes.FeedbackCards}>*/}
+        {/*        <FeatureOne comments={this.props.itemDetails.features}/>*/}
+        {/*    </div>*/}
+        {/*    <div className={classes.FeedbackCards}>*/}
+        {/*        <FeatureTwo comments={this.props.itemDetails.features}/>*/}
 
-                {/*<div className={classes.FeedbackCardsRow}>*/}
-                {/*    <div className={classes.FeedbackCards}>*/}
-                {/*        <FeatureOne comments={this.props.itemDetails.features}/>*/}
-                {/*    </div>*/}
-                {/*    <div className={classes.FeedbackCards}>*/}
-                {/*        <FeatureTwo comments={this.props.itemDetails.features}/>*/}
+        {/*    </div>*/}
+        {/*    <div className={classes.FeedbackCards}>*/}
+        {/*        <FeatureThree comments={this.props.itemDetails.features}/>*/}
 
-                {/*    </div>*/}
-                {/*    <div className={classes.FeedbackCards}>*/}
-                {/*        <FeatureThree comments={this.props.itemDetails.features}/>*/}
-
-                {/*    </div>*/}
-                {/*</div>*/}
-
-            </React.Fragment>
-        )
-    };
-
-
-
-
-
-
+        {/*    </div>*/}
+        {/*</div>*/}
+      </React.Fragment>
+    );
+  }
 }
 
 export default DonetChart;
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,39 +1,29 @@
 import React, { Component } from "react";
 
-import classes from './Layout.module.css';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-// import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import classes from "./Layout.module.css";
 
 class Layout extends Component {
-    state = {
-        showSideDrawer: false
-    }
+  state = {
+    showSideDrawer: false,
+  };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
-    }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return { showSideDrawer: !this.state.showSideDrawer };
-        });
-    }
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !this.state.showSideDrawer };
+    });
+  };
 
-
-    render() {
-        return (
-            <React.Fragment>
-                {/*<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>*/}
-                {/*<SideDrawer*/}
-                {/*    open={this.state.showSideDrawer}*/}
-                {/*    closed={this.sideDrawerClosedHandler}/>*/}
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-                {/*<hr style={{marginTop: "100vh", opacity: "0"}}/>*/}
-            </React.Fragment>
-        )
-    }
-};
+  render() {
+    return (
+      <React.Fragment>
+        <main className={classes.Content}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Layout;
